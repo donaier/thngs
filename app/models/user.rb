36a77e734fe31a9f375b40ea80585c0f8bee79d1
donat baier <donat.baier@gmail.com>
2021-01-22 10:47:24 +0100

    userconf

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :confirmable, :lockable, :timeoutable, :trackable, :omniauthable, :registerable, :recoverable, :rememberable,
+  devise :database_authenticatable, :validatable
 end
