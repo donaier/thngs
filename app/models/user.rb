612d6c919fbb400d79fc430652a5e4bc4ae7dd11
donat baier <donat.baier@gmail.com>
2021-01-22 11:07:01 +0100

    lists

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable, :omniauthable, :registerable, :recoverable, :rememberable,
   devise :database_authenticatable, :validatable
+
+  has_many :lists, foreign_key: 'owner'
 end
