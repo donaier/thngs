50fd7308348183ce71b81c3c799ef7b0b1510ee7
donat baier <donat.baier@gmail.com>
2021-04-11 01:53:08 +0200

    board nav

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def home
-    @board = current_user.home_board
+    if params[:board]
+      @home_board = Board.find(params[:board])
+    else
+      @home_board = current_user.home_board
+    end
   end
 
   def profile
