01ac4584d799b7b1c9b1b2db8895168d8e2129bd
donat baier <donat.baier@gmail.com>
2021-04-10 20:47:48 +0200

    list adding

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,30 +1,22 @@
 class ListsController < ApplicationController
   before_action :set_list, only: [:show, :edit, :update, :destroy]
 
-  # GET /lists
-  # GET /lists.json
   def index
     @lists = List.all
     @orphanTasks = Task.orphans.listed
     @orphanLnks = Lnk.orphans
   end
 
-  # GET /lists/1
-  # GET /lists/1.json
   def show
   end
 
-  # GET /lists/new
   def new
     @list = List.new
   end
 
-  # GET /lists/1/edit
   def edit
   end
 
-  # POST /lists
-  # POST /lists.json
   def create
     @list = List.new(list_params)
 
@@ -39,8 +31,6 @@ class ListsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /lists/1
-  # PATCH/PUT /lists/1.json
   def update
     respond_to do |format|
       if @list.update(list_params)
@@ -53,8 +43,6 @@ class ListsController < ApplicationController
     end
   end
 
-  # DELETE /lists/1
-  # DELETE /lists/1.json
   def destroy
     @list.destroy
     respond_to do |format|
@@ -64,7 +52,6 @@ class ListsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_list
       @list = List.find(params[:id])
     end
